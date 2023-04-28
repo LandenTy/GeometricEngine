def4ab8c0bc5272246446d1495e974826540abea
LandenTy <93301002+LandenTy@users.noreply.github.com>
2023-04-27 19:14:12 -0500

    Update Camera.py

--- Editor/Camera.py
+++ Editor/Camera.py
@@ -3,7 +3,7 @@ Camera
 
 Description:
 """
-import pygame, math
+import pygame,math,time
 pygame.init()
 
 # Functions
@@ -16,9 +16,11 @@ def rotate2d(pos, rad):
 # Classes
 class Camera:
     
-    def __init__(self, pos=(0, 0, 0), rot=(0,0)):
+    def __init__(self,pos=(0, 0, 0),rot=(0,0),speed=1,jumpHeight=5):
         self.pos = list(pos)
         self.rot = list(rot)
+        self.speed = speed
+        self.jumpHeight = jumpHeight
     
     def Mouse_Controller(self, event):
         if event.type == pygame.MOUSEMOTION:
@@ -43,31 +45,46 @@ class Camera:
         s = dt * 3 # X Rotation
         
         # Flying
-        if key[pygame.K_e]: self.pos[1] += s
-        if key[pygame.K_q]: self.pos[1] -= s
+        if key[pygame.K_q]: self.pos[1] += s
+        if key[pygame.K_e]: self.pos[1] -= s
         
         # Normalize Rotation
-        x,y = s * math.sin(self.rot[1]), s * math.cos(self.rot[1])
+        x,z = s * math.sin(self.rot[1]), s * math.cos(self.rot[1])
         
         # Arrows
         if key[pygame.K_w]: # Forward
-            self.pos[0] += x
-            self.pos[2] += y
-        
+            self.pos[0] += x * self.speed
+            self.pos[2] += z * self.speed
+            
         if key[pygame.K_s]: # Backward
-            self.pos[0] -= x
-            self.pos[2] -= y
+            self.pos[0] -= x * self.speed
+            self.pos[2] -= z * self.speed
             
         if key[pygame.K_a]: # Strafe Left
-            self.pos[0] -= y
-            self.pos[2] += x
+            self.pos[0] -= z * self.speed
+            self.pos[2] += x * self.speed
             
         if key[pygame.K_d]: # Strafe Right
-            self.pos[0] += y
-            self.pos[2] -= x
+            self.pos[0] += z * self.speed
+            self.pos[2] -= x * self.speed
+        
+        # Running (LShift & RShift)
+        if pygame.key.get_mods() == 2: self.speed = 2
+        else: self.speed = 1
+        
+        if key[pygame.K_SPACE]: # Jumping
+            print("Jumping!")
     
 class Scene:
         
     def CreateScene(sceneList, (w,h)):
         scene = pygame.display.set_mode([w,h])
         return scene
+
+class Material:
+    
+    def __init__(self, (r,g,b)):
+        self.mat = tuple([r,g,b])
+        
+    def color(self):
+        return self.mat
